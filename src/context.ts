75096d6ddb7730e46a85185f399a3d28f0745748
Kristian Dupont <kristian@kristiandupont.com>
2026-05-21 06:49:08 +0000

    Remove trpc dependency

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext, useContext } from "react";
-import type { UseTRPCMutationResult } from "@trpc/react-query/shared";
 import type { WorkspaceDelta } from "./types";
 import { snakeToCamelPlural } from "./utils";
 
@@ -62,66 +61,10 @@ export function createWorkspaceContext<T>() {
     return fn;
   }
 
-  function useMutationWithDelta<TData, TError, TVariables, TContext>(
-    mutation: UseTRPCMutationResult<
-      { data: TData; delta: WorkspaceDelta },
-      TError,
-      TVariables,
-      TContext
-    >,
-  ): Omit<
-    UseTRPCMutationResult<TData, TError, TVariables, TContext>,
-    "mutate" | "mutateAsync" | "data"
-  > & {
-    mutate: (
-      variables: TVariables,
-      options?: Parameters<
-        UseTRPCMutationResult<TData, TError, TVariables, TContext>["mutate"]
-      >[1],
-    ) => void;
-    mutateAsync: (
-      variables: TVariables,
-      options?: Parameters<
-        UseTRPCMutationResult<
-          TData,
-          TError,
-          TVariables,
-          TContext
-        >["mutateAsync"]
-      >[1],
-    ) => Promise<TData>;
-    data: TData | undefined;
-  } {
-    const applyDelta = useApplyDelta();
-    return {
-      ...mutation,
-      data: mutation.data?.data,
-      mutate: (variables, options) => {
-        mutation.mutate(variables, {
-          ...options,
-          onSuccess: (
-            data: { data: TData; delta: WorkspaceDelta },
-            vars: TVariables,
-            ctx: TContext,
-          ) => {
-            applyDelta(data.delta);
-            options?.onSuccess?.(data.data, vars, ctx);
-          },
-        } as any);
-      },
-      mutateAsync: async (variables, options) => {
-        const result = await mutation.mutateAsync(variables, options as any);
-        applyDelta(result.delta);
-        return result.data;
-      },
-    };
-  }
-
   return {
     workspaceContext,
     applyDeltaContext,
     useWorkspace,
     useApplyDelta,
-    useMutationWithDelta,
   };
 }
